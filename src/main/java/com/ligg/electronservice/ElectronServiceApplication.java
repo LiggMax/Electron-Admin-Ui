1445b8e2a64832cf18b65dba6827407378b50529
Ligg <wenzhouli06@mail.com>
2025-03-18 18:49:02 +0800

    初始化后端

---
+++ src/main/java/com/ligg/electronservice/ElectronServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ligg.electronservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ligg/electronservice/ElectronServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ligg.electronservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
