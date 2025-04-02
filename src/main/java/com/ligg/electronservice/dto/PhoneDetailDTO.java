4b5f7979d51e2082dd62675b2dcbbb4157f4406a
Ligg <wenzhouli06@mail.com>
2025-04-02 15:09:54 +0800

    update

--- src/main/java/com/ligg/electronservice/dto/PhoneDetailDTO.java
+++ src/main/java/com/ligg/electronservice/dto/PhoneDetailDTO.java
@@ -1,7 +1,12 @@
 package com.ligg.electronservice.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
 public class PhoneDetailDTO {
     private Integer phoneId;
     private Long phoneNumber;
@@ -13,68 +18,4 @@ public class PhoneDetailDTO {
     private String projectName;
     private LocalDateTime timeOfUse;
 
-    // Getters and Setters
-    public Integer getPhoneId() {
-        return phoneId;
-    }
-
-    public void setPhoneId(Integer phoneId) {
-        this.phoneId = phoneId;
-    }
-
-    public Long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(Long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
-
-    public Integer getLineStatus() {
-        return lineStatus;
-    }
-
-    public void setLineStatus(Integer lineStatus) {
-        this.lineStatus = lineStatus;
-    }
-
-    public Integer getUsageStatus() {
-        return usageStatus;
-    }
-
-    public void setUsageStatus(Integer usageStatus) {
-        this.usageStatus = usageStatus;
-    }
-
-    public LocalDateTime getRegistrationTime() {
-        return registrationTime;
-    }
-
-    public void setRegistrationTime(LocalDateTime registrationTime) {
-        this.registrationTime = registrationTime;
-    }
-
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public LocalDateTime getTimeOfUse() {
-        return timeOfUse;
-    }
-
-    public void setTimeOfUse(LocalDateTime timeOfUse) {
-        this.timeOfUse = timeOfUse;
-    }
-} 
+}
